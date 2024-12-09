4bc6842dea65752f14dd17fd9e6fd386f0c5ee67
DEDSIDE <georguzitaev@gmail.com>
2024-12-09 21:05:17 +0300

    сделал изменение 1

--- Game2048/FormGame.cs
+++ Game2048/FormGame.cs
@@ -12,6 +12,7 @@ namespace Game2048
 {
     public partial class FormGame : Form
     {
+        //изменение 1
         int len = DataBase.len;
         static int[,] gameField, savedField;
         static int[] values = new int[1000];
